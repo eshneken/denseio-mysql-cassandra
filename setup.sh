752cbbd9734afc921e95fea6834a39af74f261ce
Ed Shnekendorf <eshneken@gmail.com>
2022-08-30 11:40:15 -0400

    add mysql config vars

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-
 #
 # MySQL and Kafka on DenseIO 32 OCPU
 #
@@ -6,7 +5,7 @@
 # Assumptions:
 # OL7.9 on DenseIO-32
 # Two regions peered together
-# Ports 22, 7000 (cassandra), 3
+# Ports 22, 7000 (cassandra), 3306 (mysql)
 
 #
 # docker install
@@ -32,23 +31,32 @@ sudo mdadm --create /dev/md0 --raid-devices=4 --level=0 /dev/nvme0n1 /dev/nvme1n
 sudo mkfs.ext4 -F /dev/md0
 sudo mkdir /u01
 sudo mount /dev/md0 /u01
+sudo chown -R opc:opc /u01
 echo "/dev/md0    /u01  ext4    defaults    0 0" | sudo tee -a /etc/fstab > /dev/null
 
 #
 # Run MySQL
+# https://phoenixnap.com/kb/mysql-docker-container
 #
-sudo mkdir /u01/mysql-data
-sudo chown opc /u01/mysql-data/
-sudo chgrp opc /u01/mysql-data/
-sudo docker run --name mysql -d -p 3306:3306 --cpus=8 --memory=16g  -e MYSQL_ROOT_PASSWORD=BoedyPoCPwd123! -v /u01/mysql-data:/var/lib/mysql:rw docker.io/mysql:8
+# To test config variables get picked up:
+# 1. sudo yum -y install mysql
+# 2. mysql -uroot -pBoedyPoCPwd123! -h127.0.0.1 -P3306 -e 'show global variables';
+#
+mkdir /u01/mysql-data
+mkdir /u01/mysql-config
+
+echo "[mysqld]" | tee -a /u01/mysql-config/my.cnf > /dev/null
+echo "innodb_buffer_pool_size=10737418240" | tee -a /u01/mysql-config/my.cnf > /dev/null
+echo "innodb_flush_method=O_DIRECT" | tee -a /u01/mysql-config/my.cnf > /dev/null
+echo "innodb_doublewrite=1" | tee -a /u01/mysql-config/my.cnf > /dev/null
+
+sudo docker run --name mysql -d -p 3306:3306 --cpus=8 --memory=16g  -e MYSQL_ROOT_PASSWORD=BoedyPoCPwd123! -v /u01/mysql-config:/etc/mysql/conf.d:rw -v /u01/mysql-data:/var/lib/mysql:rw docker.io/mysql:8
 
 #
 # Run Cassandra
 # https://hub.docker.com/_/cassandra
 #
-sudo mkdir /u01/cassandra-data
-sudo chown opc /u01/cassandra-data/
-sudo chgrp opc /u01/cassandra-data/
+mkdir /u01/cassandra-data
 
 # first host in ring
 #sudo docker run --name cassandra -d -p 7000:7000 --cpus=24 --memory=50g -e CASSANDRA_BROADCAST_ADDRESS=10.0.0.145 -v /u01/cassandra-data:/var/lib/cassandra cassandra:3.0.27
