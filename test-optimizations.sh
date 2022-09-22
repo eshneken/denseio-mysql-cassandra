48d588cfb56fe32099171be6f7f59824d5f8e576
Ed Shnekendorf <eshneken@gmail.com>
2022-09-22 10:26:40 -0400

    added docker directory move

--- test-optimizations.sh
+++ test-optimizations.sh
@@ -31,10 +31,25 @@ sudo reboot
 cat /proc/cmdline
 
 ##
-## move docker from block to local storage.  Do this only on the database server
+## Move docker from block to local storage.  Do this only on the database server
 ##
 ## https://linuxconfig.org/how-to-move-docker-s-default-var-lib-docker-to-another-directory-on-ubuntu-debian-linux
 ##
+sudo systemctl stop docker.service
+sudo systemctl stop docker.socket
+sudo vi /lib/systemd/system/docker.service
+
+## MANUAL STEP
+##
+## Modify ExecStart line to read:  ExecStart=/usr/bin/dockerd -g /u01/docker-home -H fd:// --containerd=/run/containerd/containerd.sock
+##
+
+sudo mkdir -p /u01/docker-home
+sudo rsync -aqxP /var/lib/docker/ /u01/docker-home/
+sudo systemctl daemon-reload
+sudo systemctl start docker
+sudo docker start mysql
+sudo docker ps
 
 ## 
 ## apply sysctl changes
